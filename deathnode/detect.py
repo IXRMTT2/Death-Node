bdfbb119b2c6799a7ec6216f73328c2fc5abcc45
IXRMTT <ixrgithub@gmail.com>
2025-03-12 11:30:12 +0000

    detect update + sniff update

---
+++ deathnode/detect.py
@@ -0,0 +1,31 @@
+from sklearn.ensemble import IsolationForest
+import numpy as np
+import time
+
+def detect_anomalies(packet_count):
+    model = IsolationForest(contamination=0.1)
+    model.fit(packet_count)
+    predictions = model.predict(packet_count)
+
+    for i, pred in enumerate(predictions):
+        if pred == -1: 
+            alert_user(i, packet_count[i])
+
+def alert_user(index, data):
+    print("[IMPORTANT] Possible Threat Detected")
+    print(f"Suspicious Activity Detected at index {index}: {data}")
+    print("Options: (1) Block | (2) Deauth | (3) Ignore")
+
+    action = input ("Choose Action: ")
+    if action == "1":
+        mac = input("Enter MAC Address to Block: ")
+        block_mac(mac)
+    elif action == "2":
+        mac = input("Enter MAC Address to Write in the Death Node: ")
+        gateway_mac = input("Enter Gateway MAC Address: ")
+        deauth(mac, gateway_mac)
+    elif action == "3":
+        print("[INFO] Ignoring the Threat")
+    else:
+        print("[ERROR] Invalid Choice")
+
--- deathnode/sniff.py
+++ deathnode/sniff.py
@@ -1,9 +1,19 @@
 from scapy.all import *
 from scapy.all import Dot11, sniff
 import time 
+from detect import detect_anomalies
+import numpy as np
+
+packet_count = []
 
 def packet_handler(pkt):
     if pkt.haslayer(Dot11):
+        packet_count.append([len(pkt)])
+        if len(packet_count) >= 10:
+            detect_anomalies(np.array(packet_count))
+
+        print(f"[+] Detected Packet: {len(pkt)}")
+        
         mac = pkt.addr2
         with open("logs/threats.log", "a") as log:
             log.write(f"{time.ctime()} - Detected Packet from {mac}\n")
